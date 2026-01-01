33777b6b069111aa5b278a0022d41d18bde195be
admin <fatimahaminati@gmail.com>
2026-01-01 14:04:56 +0700

    menambahkan modul 8 & 9

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', blog_views.custom_logout, name='logout'),
     path('', include('blog.urls')),
 ]
